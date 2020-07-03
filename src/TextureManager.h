bb9a56ae01d000fc76de8d86a7c0c6b3221b0523
Your Name <you@example.com>
2020-07-03 13:48:08 -0500

    TextureManager added

---
+++ src/TextureManager.h
@@ -0,0 +1,16 @@
+#ifndef TEXTUREMANAGER_H
+#define TEXTUREMANAGER_H
+
+#include "./Game.h"
+
+class TextureManager {
+    public:
+        static SDL_Texture* LoadTexture(const char* fileName);
+        static void Draw(
+            SDL_Texture* texture,
+            SDL_Rect sourceRect,
+            SDL_Rect destinationRect,
+            SDL_RendererFlip flip);
+};
+
+#endif
